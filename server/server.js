140ea38eaa1e79d54c3185588090dad5ca9acb30
Bilal <56759528+Bilal-tamerhoulet@users.noreply.github.com>
2021-02-17 10:37:49 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,12 +24,6 @@ app.use(
   })
 );
 
-app.use(function(req, res, next) {
-      if ((req.get('X-Forwarded-Proto') !== 'https')) {
-        res.redirect('https://' + req.get('Host') + req.url);
-      } else
-        next();
-    });
 
 app.get("/", (req, res) => {
   const filePath = path.resolve(process.env.STATIC_DIR + "/index.html");
